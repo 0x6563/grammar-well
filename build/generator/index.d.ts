5ae0bbbf4097d104095163d01f97b4b9ea57e109
0x6563 <63897158+0x6563@users.noreply.github.com>
2024-06-23 11:34:15 -0400

    export grammars and resolvers

--- build/generator/index.d.ts
+++ build/generator/index.d.ts
@@ -1,2 +1,6 @@
 export * from './generator';
 export * from './import-resolvers/default';
+export * from './import-resolvers/browser';
+export * from './import-resolvers/filesystem';
+export { default as V1Grammar } from './grammars/v1';
+export { default as V2Grammar } from './grammars/v2';
--- build/generator/index.js
+++ build/generator/index.js
@@ -1,3 +1,7 @@
 export * from './generator';
 export * from './import-resolvers/default';
+export * from './import-resolvers/browser';
+export * from './import-resolvers/filesystem';
+export { default as V1Grammar } from './grammars/v1';
+export { default as V2Grammar } from './grammars/v2';
 //# sourceMappingURL=index.js.map
