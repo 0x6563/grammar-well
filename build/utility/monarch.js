bc3fca19560c77a15c3252eab8a691b5f12008fc
0x6563 <63897158+0x6563@users.noreply.github.com>
2024-07-07 17:00:40 -0400

    add unmatched rule to tokenizer

--- build/utility/monarch.js
+++ build/utility/monarch.js
@@ -2,35 +2,41 @@ export function CreateMonarchTokenizer(lexer) {
     const tokenizer = {};
     const { start, states } = lexer;
     for (const name in states) {
-        const { rules } = states[name];
+        const { rules, unmatched } = states[name];
         tokenizer[name] = [];
         for (const rule of rules) {
-            const action = { token: rule.highlight || 'source' };
-            if ('pop' in rule) {
-                action.next = '@pop';
-            }
-            else if ('goto' in rule) {
-                action.next = '@' + rule.goto;
-            }
-            else if ('set' in rule) {
-                action.switchTo = '@' + rule.set;
-            }
-            else if ('inset' in rule) {
-                action.next = '@push';
-            }
-            if ('embed' in rule) {
-                action.nextEmbedded = rule.embed;
-            }
-            else if ('unembed' in rule) {
-                action.nextEmbedded = '@pop';
-            }
-            if (rule.before) {
-                action.token = '@rematch';
-            }
-            tokenizer[name].push([TransformWhen(rule.when), action]);
+            tokenizer[name].push([TransformWhen(rule.when), CreateAction(rule)]);
+        }
+        if (unmatched) {
+            tokenizer[name].push([/.*/, CreateAction(unmatched)]);
         }
     }
     return { start, tokenizer };
+}
+function CreateAction(rule) {
+    const action = { token: rule.highlight || 'source' };
+    if ('pop' in rule) {
+        action.next = '@pop';
+    }
+    else if ('goto' in rule) {
+        action.next = '@' + rule.goto;
+    }
+    else if ('set' in rule) {
+        action.switchTo = '@' + rule.set;
+    }
+    else if ('inset' in rule) {
+        action.next = '@push';
+    }
+    if ('embed' in rule) {
+        action.nextEmbedded = rule.embed;
+    }
+    else if ('unembed' in rule) {
+        action.nextEmbedded = '@pop';
+    }
+    if (rule.before) {
+        action.token = '@rematch';
+    }
+    return action;
 }
 function TransformWhen(obj) {
     return typeof obj == 'string' ? new RegExp(RegexEscape(obj)) : obj;
--- src/utility/monarch.ts
+++ src/utility/monarch.ts
@@ -1,38 +1,43 @@
-import { RuntimeLexerConfig, RuntimeLexerStateMatchRule } from "../typings/index.js";
+import { ASTLexerStateNonMatchRule, RuntimeLexerConfig, RuntimeLexerStateMatchRule } from "../typings/index.js";
 
 export function CreateMonarchTokenizer(lexer: RuntimeLexerConfig) {
     const tokenizer: any = {}; // languages.IMonarchLanguage['tokenizer']
     const { start, states } = lexer;
     for (const name in states) {
-        const { rules } = states[name];
+        const { rules, unmatched } = states[name];
         tokenizer[name] = [];
         for (const rule of rules) {
-
-            const action: any = { token: rule.highlight || 'source' };
-            if ('pop' in rule) {
-                action.next = '@pop';
-            } else if ('goto' in rule) {
-                action.next = '@' + rule.goto;
-            } else if ('set' in rule) {
-                action.switchTo = '@' + rule.set;
-            } else if ('inset' in rule) {
-                action.next = '@push';
-            }
-
-            if ('embed' in rule) {
-                action.nextEmbedded = rule.embed;
-            } else if ('unembed' in rule) {
-                action.nextEmbedded = '@pop';
-            }
-
-            if (rule.before) {
-                action.token = '@rematch';
-            }
-
-            tokenizer[name].push([TransformWhen(rule.when), action])
+            tokenizer[name].push([TransformWhen(rule.when), CreateAction(rule)])
+        }
+        if (unmatched) {
+            tokenizer[name].push([/.*/, CreateAction(unmatched)]);
         }
     }
     return { start, tokenizer };
+}
+
+function CreateAction(rule: RuntimeLexerStateMatchRule | ASTLexerStateNonMatchRule) {
+    const action: any = { token: rule.highlight || 'source' };
+    if ('pop' in rule) {
+        action.next = '@pop';
+    } else if ('goto' in rule) {
+        action.next = '@' + rule.goto;
+    } else if ('set' in rule) {
+        action.switchTo = '@' + rule.set;
+    } else if ('inset' in rule) {
+        action.next = '@push';
+    }
+
+    if ('embed' in rule) {
+        action.nextEmbedded = rule.embed;
+    } else if ('unembed' in rule) {
+        action.nextEmbedded = '@pop';
+    }
+
+    if (rule.before) {
+        action.token = '@rematch';
+    }
+    return action;
 }
 
 function TransformWhen(obj: RuntimeLexerStateMatchRule['when']) {
